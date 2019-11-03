58bb52f827ac4917a9facd25ce8b776527b0d046
arakawan <arakawan@pivot.jp>
2019-11-03 21:56:54 +0900

    142ページまで

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
+import Rect from './Rect';
 import './App.css';
 
-
 class App extends Component {
-
-
-  constructor(props){
-    super();
-    this.title = props.title;
-    this.message = props.message;
-  }
   render(){
     return <div>
-      <h1>{this.title}</h1>
-      <p>{this.message}</p>
-  </div>;
+      <h1>React</h1>
+      <Rect x="50" y="50" w="150" h="150" c="cyan"  r = '50'/>
+      <Rect x="150" y="100" w="150" h="150" c="magenta"  r = '75'/>
+      <Rect x="100" y="150" w="150" h="150" c="black" r = '25'/>
+    </div>;
   }
 }
-
 
 export default App;
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App title='App' message='This is App Component!' />,
-  document.getElementById('root')
+  <App />,　document.getElementById('root')
 )
 
 serviceWorker.unregister();
