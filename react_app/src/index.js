342a07960813a453df98f70126138008a60a1864
arakawan <arakawan@pivot.jp>
2019-11-03 21:37:51 +0900

    136ページまで

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <App title='App' message='This is App Component!' />,
+  document.getElementById('root')
+)
 
 serviceWorker.unregister();
