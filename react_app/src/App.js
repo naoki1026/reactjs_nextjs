0dfde190cae094c36787a73a2ca1d5c49e300082
arakawan <arakawan@pivot.jp>
2019-11-04 13:50:35 +0900

    174ページまで

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,83 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Rect from './Rect';
 import './App.css';
-
 
 class App extends Component {
   input = '';
 
-
   msgStyle = {
-    fontSize:"20pt",
-    color:"#900",
-    margin:"20px 0px",
-    padding: "5px",
+    fontSize: '20pt',
+    color: '#900',
+    margin: '20px 0px',
+    padding: '5px',
   }
-
-
+  inputStyle = {
+    fontSize: '12pt',
+    padding: '5px',
+  }
   constructor(props){
     super(props);
     this.state = {
-      message:'type your name:'
+      message: 'type your name: '
     };
     this.doChange = this.doChange.bind(this);
     this.doSubmit = this.doSubmit.bind(this);
   }
 
-
-  doChange(event) {
+  doChange(event){
     this.input = event.target.value;
   }
 
-
-  doSubmit(event) {
+  doSubmit(event){
     this.setState({
-      message: 'Hello, ' + this.input + '!!'
+      message: 'Hello ' + this.input + '!!'
     });
     event.preventDefault();
   }
   render(){
     return <div>
       <h1>React</h1>
-      <Message title="Children!">
-        これはコンポーネント内のコンテンツです。
-        マルでテキストを分割し、リストにして表示します。
-        改行は必要ありません。
-      </Message>
+      <h2>{this.state.message}</h2>
+      <form onSubmit={this.doSubmit}>
+        <label>
+          <span style={this.inputStyle}></span>Message:
+          <input type='text' style={this.inputStyle}
+          onChange={this.doChange}
+          required pattern="[A-Za-z _,.]+" />
+          {/* required pattern="[A-Za-z _,.]+" /> */}
+        </label>
+        <input type='submit' style={this.inputStyle} value='Click' />
+      </form>
     </div>;
   }
-
-
 }
-
-
-class Message extends Component {
-  li = {
-    fontSize:"16pt",
-    color:"#06",
-    margin:"0px",
-    padding: "0px",
-  }
-
-
-  render(){
-    let content = this.props.children;
-    let arr = content.split('。');
-    let arr2 = [];
-    for(let i = 0;i < arr.length;i++){
-      if (arr[i].trim() != ''){
-        arr2.push(arr[i]);
-      }
-    }
-    let list = arr2.map((value,key)=>(
-      <li style={this.li} key={key}>{value}.</li>)
-    );
-    return <div>
-      <h2>{this.props.title}</h2>
-      <ol>{list}</ol>
-    </div>
-  }
-}
-
 
 export default App;
